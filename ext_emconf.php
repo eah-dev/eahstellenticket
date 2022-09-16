008f7ccfa02d925f81f58f957bde733fdab00d3d
hoelbing <carsten@hoelbing.net>
2022-09-16 15:28:12 +0200

    add icon,  remove unused icons, update title and version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,12 @@
  */
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'EAHJena stellenticket',
+    'title' => 'EAH Jena - Stellenticket',
     'description' => 'This package read a json file from stellenticket',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-10.5.99',
         ],
         'conflicts' => [
         ],
@@ -33,5 +33,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Carsten Hoelbing',
     'author_email' => 'carsten.hoelbing@eah-jena.de',
     'author_company' => 'Ernst-Abbe-Hochschule Jena',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
 ];
